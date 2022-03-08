48eb7bb91c5e6cf315a966c53a0bdaf9ba23162b
Nisha Paudel Pandey <nshpoudel263@gmail.com>
2022-03-08 17:29:28 +1100

    layout is added

--- src/App.js
+++ src/App.js
@@ -1,11 +1,14 @@
+import React from "react";
 import "./App.css";
+import { DefaultLayout } from "./components/layout/DefaultLayout";
 
 import { Entry } from "./pages/Entry/Entry.page";
 
 function App() {
   return (
     <div className="App">
-      <Entry />
+      {/* <Entry /> */}
+      <DefaultLayout>//Dashboard</DefaultLayout>
     </div>
   );
 }

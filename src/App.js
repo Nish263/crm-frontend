ed845edf9a0bd8d6cede5298e8be297ef0deca0a
Nisha Paudel Pandey <nshpoudel263@gmail.com>
2022-03-18 16:12:30 +1100

    added ticket list page

--- src/App.js
+++ src/App.js
@@ -2,9 +2,10 @@ import React from "react";
 import "./App.css";
 import { DefaultLayout } from "./components/layout/DefaultLayout";
 // import { Dashboard } from "./pages/Entry/dashboard/Dashboard.page";
-import { AddTicket } from "./pages/new-ticket/AddTicket.page";
+// import { AddTicket } from "./pages/new-ticket/AddTicket.page";
 
-import { Entry } from "./pages/Entry/Entry.page";
+// import { Entry } from "./pages/Entry/Entry.page";
+import { TicketList } from "./pages/ticket-list/TicketList.page";
 
 function App() {
   return (
@@ -12,7 +13,8 @@ function App() {
       {/* <Entry /> */}
       <DefaultLayout>
         {/* <Dashboard /> */}
-        <AddTicket />
+        {/* <AddTicket /> */}
+        <TicketList />
       </DefaultLayout>
     </div>
   );
---
+++ src/pages/ticket-list/TicketList.page.js
@@ -0,0 +1,51 @@
+import React, { useState, useEffect } from "react";
+import { Container, Row, Col, Button } from "react-bootstrap";
+import { PagesBreadcrumb } from "../../components/breadcrumb/PagesBreadcrumb.comp";
+import { SearchForm } from "../../components/search-form/SearchForm.comp";
+import { TicketTable } from "../../components/ticket-table/TicketTable.com";
+import tickets from "../../assests/Data/dummy-tickets.json";
+
+export const TicketList = () => {
+  const [str, setStr] = useState("");
+  const [dispTicket, setDispTicket] = useState([]);
+
+  useEffect(() => {}, [str, dispTicket]);
+
+  const handleOnChange = (e) => {
+    const { value } = e.target;
+
+    setStr(value);
+    searchTicket(value);
+  };
+
+  const searchTicket = (sttr) => {
+    const displayTickets = tickets.filter((row) =>
+      row.subject.toLowerCase().includes(sttr.toLowerCase())
+    );
+
+    setDispTicket(displayTickets);
+  };
+  return (
+    <Container>
+      <Row>
+        <Col>
+          <PagesBreadcrumb pages="Ticket List" />
+        </Col>
+      </Row>
+      <Row className="mt-4">
+        <Col>
+          <Button variant="info"> Add New Ticket </Button>
+        </Col>
+        <Col className="text-right">
+          <SearchForm handleOnChange={handleOnChange} str={str} />
+        </Col>
+      </Row>
+      <hr />
+      <Row>
+        <Col>
+          <TicketTable tickets={dispTicket} />
+        </Col>
+      </Row>
+    </Container>
+  );
+};
